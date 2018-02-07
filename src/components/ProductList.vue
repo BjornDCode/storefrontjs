eefa7937d65b62d8ba744e4402c33cd0736d2f28
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-02-07 13:45:23 +0100

    Product List

---
+++ src/components/ProductList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="product-list">
+        
+        <slot name="header"></slot>   
+
+        <slot v-for="product in products" :product="product">
+            <sf-product-card  :product="product"></sf-product-card>
+        </slot>
+
+        <slot name="footer"></slot>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            products: {
+                type: Array,
+                required: true
+            }
+        }
+    }
+</script>
--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import ProductCardPrice from './components/ProductCardPrice.vue';
 import ProductCardTitle from './components/ProductCardTitle.vue';
 import ProductCardDescription from './components/ProductCardDescription.vue';
 
+import ProductList from './components/ProductList.vue';
+
 let Storefront = {
 
     install(Vue, options) {
@@ -28,6 +30,8 @@ let Storefront = {
         Vue.component('sf-product-card-price', ProductCardPrice);
         Vue.component('sf-product-card-title', ProductCardTitle);
         Vue.component('sf-product-card-description', ProductCardDescription);
+
+        Vue.component('sf-product-list', ProductList);
 
     }
 
