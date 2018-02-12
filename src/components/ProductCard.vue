4a0408651d9fda8cc74cd1b0a1ca6313bc0c89da
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-02-12 13:25:51 +0100

    Show the right product properties

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="product-card">
-        <sf-product-card-image :image="product.image"></sf-product-card-image>
+    <div class="product-card" v-if="product">
+        <sf-product-card-image :image="product.images[0]"></sf-product-card-image>
         <div class="product-card__info">
             <sf-product-card-title :title="product.title"></sf-product-card-title>
-            <sf-product-card-price :price="product.price"></sf-product-card-price>
+            <sf-product-card-price :price="product.variants[0].price"></sf-product-card-price>
             <sf-product-card-description :description="product.description"></sf-product-card-description>
         </div>
     </div>
